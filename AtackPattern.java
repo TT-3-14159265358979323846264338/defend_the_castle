12015ca5e15e72180768f537e92b7536dd7f0599
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-08-28 12:44:09 +0900

    Add files via upload

--- AtackPattern.java
+++ AtackPattern.java
@@ -14,15 +14,21 @@ public abstract class AtackPattern {
 		this.candidate = candidate;
 	}
 	
-	//攻撃パターン説明
+	/**
+	 * 攻撃パターン説明。
+	 * @return 攻撃パターン説明を返却する。表示させるときの都合上、単語・短文が望ましい。(表示時はフォントサイズが自動調整される。)
+	 */
 	public abstract String getExplanation();
 	
-	//targetになる相手のBattleDataを返す
+	/**
+	 * targetとなる相手のBattleDataを算出する。
+	 * @return candidateを元に、条件に合うターゲットを返却する。同クラス下部に、条件を探す際によく使うメソッドがある。
+	 */
 	public abstract List<BattleData> getTarget();
 	
 	//ここから下はfilterやsortedの条件
 	protected boolean activeCheck(BattleData data){
-		return data.getActivate();
+		return data.canActivate();
 	}
 	
 	protected boolean rangeCheck(BattleData data) {
