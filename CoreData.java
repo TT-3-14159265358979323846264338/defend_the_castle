0ebae853b0680c5d7e940b2cde3bf3acf79c10ea
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-08-29 14:28:35 +0900

    Add files via upload

--- CoreData.java
+++ CoreData.java
@@ -44,25 +44,25 @@ public abstract class CoreData {
 	
 	/**
 	 * コアのレアリティ。
-	 * @return コアのレアリティを返却する。1以上の値をとり、一定の値まで到達したらDisplaySortの表示位置を再調整すること。
+	 * @return コアのレアリティを返却する。1以上の値をとり、一定の値まで到達したら{@link screendisplay.SortPanel SortPanel}の表示位置を再調整すること。
 	 */
 	public abstract int getRarity();
 	
 	/**
 	 * 装備した武器のステータス上昇率(倍率上昇)。
-	 * @return DefaultUnit.CORE_WEAPON_MAPの順にステータス上昇率をリスト化。
+	 * @return {@link defaultdata.DefaultUnit#CORE_WEAPON_MAP CORE_WEAPON_MAP}の順にステータス上昇率をリスト化。
 	 */
 	public abstract List<Double> getWeaponStatus();
 	
 	/**
 	 * ユニットのステータス上昇率(倍率上昇)。
-	 * @return DefaultUnit.CORE_UNIT_MAPの順にステータス上昇率をリスト化。
+	 * @return {@link defaultdata.DefaultUnit#CORE_UNIT_MAP CORE_UNIT_MAP}の順にステータス上昇率をリスト化。
 	 */
 	public abstract List<Double> getUnitStatus();
 	
 	/**
 	 * ダメージカット率上昇量(加算上昇)。
-	 * @return DefaultUnit.ELEMENT_MAPの順にステータス上昇量をリスト化。
+	 * @return {@link defaultdata.DefaultUnit#ELEMENT_MAP ELEMENT_MAP}の順にステータス上昇量をリスト化。
 	 */
 	public abstract List<Integer> getCutStatus();
 	
@@ -71,16 +71,16 @@ public abstract class CoreData {
 	 * バフ情報を入力した複数のListを返却する。
 	 * @return List(timing, target, range, status, culculate, effect, interval, max, duration, recast)<br>
 	 * 			<br>
-	 * 			timing - 発生させるタイミングコード。Buff.発生タイミングコードで指定。CoreではHIT使用不可。<br>
-	 * 			target - 与える対象コード。Buff.発生対象コードで指定。<br>
-	 * 			range - 与える範囲コード。Buff.効果範囲コードで指定。<br>
-	 * 			status - 効果のあるステータスコード。Buff.対象ステータスコードで指定。MORALE, GAME_COST を指定した場合、targetをGAMEに指定する必要がある。<br>
-	 * 			culculate - 最終ステータスへの計算方法コード。Buff.加減乗除コードで指定。targetがGAMEであれば、MULTIPLICATION, DIVISION使用不可。<br>
+	 * 			timing - 発生させるタイミングコード。{@link battle.Buff Buff}の発生タイミングコードで指定。Coreでは{@link battle.Buff#HIT HIT}使用不可。<br>
+	 * 			target - 与える対象コード。{@link battle.Buff Buff}の発生対象コードで指定。<br>
+	 * 			range - 与える範囲コード。{@link battle.Buff Buff}の効果範囲コードで指定。<br>
+	 * 			status - 効果のあるステータスコード。{@link battle.Buff Buff}の対象ステータスコードで指定。{@link battle.Buff#MORALE MORALE}, {@link battle.Buff#GAME_COST GAME_COST} を指定した場合、targetを{@link battle.Buff#GAME GAME}に指定する必要がある。<br>
+	 * 			culculate - 最終ステータスへの計算方法コード。{@link battle.Buff Buff}の加減乗除コードで指定。targetが{@link battle.Buff#GAME GAME}であれば、{@link battle.Buff#MULTIPLICATION MULTIPLICATION}, {@link battle.Buff#DIVISION DIVISION}使用不可。<br>
 	 * 			effect - 1回あたりの効果量。intervalを指定した際でも最大値ではないので注意。<br>
-	 * 			interval - 効果の発生間隔[s]。未使用ならBuff.NONEを指定。<br>
-	 * 			max - intervalを指定した時の最大値。未使用ならBuff.NONEを指定。<br>
-	 * 			duration - 効果持続時間[s]。未使用ならBuff.NONEを指定。<br>
-	 * 			recast - スキルの再使用時間[s]。timingがSKILLの時のみ指定することがある。未使用ならBuff.NONEを指定。<br>
+	 * 			interval - 効果の発生間隔[s]。未使用なら{@link battle.Buff#NONE Buff.NONE}を指定。<br>
+	 * 			max - intervalを指定した時の最大値。未使用なら{@link battle.Buff#NONE Buff.NONE}を指定。<br>
+	 * 			duration - 効果持続時間[s]。未使用なら{@link battle.Buff#NONE Buff.NONE}を指定。<br>
+	 * 			recast - スキルの再使用時間[s]。timingが{@link battle.Buff#SKILL SKILL}の時のみ指定することがある。未使用なら{@link battle.Buff#NONE Buff.NONE}を指定。<br>
 	 * 			<br>
 	 * 			バフを保有していない場合、空のArrays.asList()を返却する。
 	 */
@@ -95,7 +95,7 @@ public abstract class CoreData {
 	/**
 	 * スキルのアイコン画像。
 	 * @param ratio - 元の画像を何倍の縮尺で取り込むか指定。
-	 * @return スキルのアイコン画像を返却する。getBuffがnullの時はnullを返却する。
+	 * @return スキルのアイコン画像を返却する。スキルを保有しないときはnullを返却する。
 	 */
 	public BufferedImage getSkillImage(double ratio) {
 		return EditImage.input(getSkillImageName(), ratio);
