c7d0f7d80383e7da1728a3bf4334187d9ef90706
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-09-26 11:22:25 +0900

    Add files via upload

--- defendthecastle/selectstage/MenuSelectStage.java
+++ defendthecastle/selectstage/MenuSelectStage.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -20,6 +22,8 @@ import defendthecastle.MainFrame;
 public class MenuSelectStage extends JPanel{
 	private JLabel stageLabel = new JLabel();
 	private JLabel informationLabel = new JLabel();
+	private JLabel clearConditionLabel = new JLabel();
+	private JLabel gameOverLabel = new JLabel();
 	private JButton returnButton = new JButton();
 	private JButton normalModeButton = new JButton();
 	private JButton hardModeButton = new JButton();
@@ -36,6 +40,8 @@ public class MenuSelectStage extends JPanel{
 		setBackground(new Color(240, 170, 80));
 		add(stageLabel);
 		add(informationLabel);
+		add(clearConditionLabel);
+		add(gameOverLabel);
 		addReturnButton(MainFrame);
 		addNormalModeButton(MainFrame);
 		addHardModeButton(MainFrame);
@@ -48,14 +54,36 @@ public class MenuSelectStage extends JPanel{
 		super.paintComponent(g);
 		setLabel(stageLabel, "ステージ選択", 10, 10, 200, 30);
 		setLabel(informationLabel, "ステージ情報", 170, 10, 200, 30);
+		setLabel(clearConditionLabel, conditionComment("勝利条件: " + DefaultStage.STAGE_DATA.get(SelectPanel.getSelelct()).getClearCondition()), 580, 350, 320, 40);
+		setLabel(gameOverLabel, conditionComment("敗北条件: " + DefaultStage.STAGE_DATA.get(SelectPanel.getSelelct()).getGameOverCondition()), 580, 400, 320, 40);
 		setButton(returnButton, "戻る", 10, 460, 150, 60);
 		setButton(normalModeButton, "normal", 580, 460, 155, 60);
 		setButton(hardModeButton, "hard", 745, 460, 155, 60);
 		setScroll(stageScroll, 10, 40, 150, 410);
 		setScroll(meritScroll, 170, 275, 400, 245);
-		setScroll(enemyScroll, 580, 40, 320, 410);
+		setScroll(enemyScroll, 580, 40, 320, 295);
 		g.drawImage(stageImage.get(SelectPanel.getSelelct()), 170, 40, this);
+		g.setColor(stageScroll.getBackground());
+		g.fillRect(580, 345, 320, 100);
 		requestFocus();
+	}
+	
+	private String conditionComment(String comment) {
+		int lastPosition = 0;
+		List<Integer> wrapPosition = new ArrayList<>();
+		Font font = new Font("ＭＳ ゴシック", Font.BOLD, 20);
+		for(int i = 0; i < comment.length(); i++) {
+			if(320 < getFontMetrics(font).stringWidth(comment.substring(lastPosition, i))) {
+				wrapPosition.add(i - 1);
+				lastPosition = i - 1;
+			}
+		}
+		if(wrapPosition.isEmpty()) {
+			return comment;
+		}
+		StringBuilder wrapComment = new StringBuilder(comment);
+		wrapPosition.stream().sorted(Comparator.reverseOrder()).forEach(i -> wrapComment.insert(i, "<br>"));
+		return wrapComment.insert(0, "<html>").toString();
 	}
 	
 	private void addReturnButton(MainFrame MainFrame) {
