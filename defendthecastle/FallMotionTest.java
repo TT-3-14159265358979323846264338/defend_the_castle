f1162c86a92fe5397d239784a549012d794e2888
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-11-27 16:45:43 +0900

    Add files via upload

--- defendthecastle/FallMotionTest.java
+++ defendthecastle/FallMotionTest.java
@@ -12,12 +12,10 @@ import org.junit.jupiter.api.Test;
 
 class FallMotionTest {
 	private FallMotion FallMotion;
-	private ScheduledExecutorService mockScheduler;
 
 	@BeforeEach
 	void setUp() {
-		FallMotion = new FallMotion();
-		mockScheduler = mock(ScheduledExecutorService.class);
+		FallMotion = spy(new FallMotion());
 	}
 	
 	/**
@@ -26,14 +24,15 @@ class FallMotionTest {
 	 */
 	@Test
 	void testFallTimerStart() {
-		ScheduledFuture<?> mockFuture = mock(ScheduledFuture.class);
-		doReturn(mockFuture).when(mockScheduler).scheduleAtFixedRate(any(Runnable.class), anyLong(), anyLong(), any(TimeUnit.class));
+		ScheduledExecutorService mockScheduler = mock(ScheduledExecutorService.class);
 		FallMotion.fallTimerStart(mockScheduler);
-		assertTrue(FallMotion.canRunTimer());
-		assertEquals(mockFuture, FallMotion.getFallFuture());
+		assertTrue(FallMotion.canStart());
+		verify(mockScheduler).scheduleAtFixedRate(any(Runnable.class), anyLong(), anyLong(), any(TimeUnit.class));
 	}
+	
 	/**
-	 * 角度と位置が変化していることを確認。
+	 * 角度と位置が変化していることを確認。<br>
+	 * タイマー停止操作が呼び出されているか確認。
 	 */
 	@Test
 	void testFallTimerProcess() {
@@ -42,5 +41,34 @@ class FallMotionTest {
 		FallMotion.fallTimerProcess();
 		assertTrue(initialAngle != FallMotion.getAngle());
 		assertTrue(initialY != FallMotion.getY());
+		verify(FallMotion).timerStop();
+	}
+	
+	/**
+	 * yの値が基準より小さければタイマーは動作を継続しているか確認。
+	 */
+	@Test
+	void testTimerStopFalse() {
+		ScheduledFuture<?> mockFuture = mock(ScheduledFuture.class);
+		FallMotion.setStart(true);
+		FallMotion.setFallFuture(mockFuture);
+		FallMotion.setY(100);
+		FallMotion.timerStop();
+		assertTrue(FallMotion.canStart());
+		verify(mockFuture, never()).cancel(true);
+	}
+	
+	/**
+	 * yの値が基準より大きければタイマーは動作を停止させたか確認。
+	 */
+	@Test
+	void testTimerStopTrue() {
+		ScheduledFuture<?> mockFuture = mock(ScheduledFuture.class);
+		FallMotion.setStart(true);
+		FallMotion.setFallFuture(mockFuture);
+		FallMotion.setY(500);
+		FallMotion.timerStop();
+		assertFalse(FallMotion.canStart());
+		verify(mockFuture).cancel(true);
 	}
 }
