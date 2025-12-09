5f6138fd5f16666a34a7348c4818d715e92c4e29
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-12-09 14:56:39 +0900

    Add files via upload

--- test/defendthecastle/MenuMainTest.java
+++ test/defendthecastle/MenuMainTest.java
@@ -2,14 +2,20 @@ package defendthecastle;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import javax.swing.JButton;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 class MenuMainTest {
 	private MenuMain MenuMain;
@@ -20,21 +26,33 @@ class MenuMainTest {
 	}
 
 	/**
-	 * 
+	 * MainFrameが更新されていることを確認。<br>
+	 * JButtonがJPanelに追加されているか確認。<br>
+	 * 全てのJButtonにActionListenersが追加されているか確認。<br>
+	 * scheduleAtFixedRateでのタイマーがセットされているか確認。
 	 */
 	@Test
 	void testMenuMain() {
 		MainFrame MainFrame = mock(MainFrame.class);
+		ScheduledExecutorService mockScheduler = mock(ScheduledExecutorService.class);
+		MockedStatic<Executors> mockExecutors = mockStatic(Executors.class);
+		mockExecutors.when(() -> Executors.newScheduledThreadPool(anyInt())).thenReturn(mockScheduler);
 		MenuMain = new MenuMain(MainFrame);
-		JButton[] button = {MenuMain.getItemGetButton(),
-				MenuMain.getItemDisposeButton(),
-				MenuMain.getCompositionButton(),
-				MenuMain.getSelectStageButton(),
-				MenuMain.getTestButton()
-		};
+		JButton[] allButton = allButton();
 		assertThat(MenuMain.getMainFrame(), is(MainFrame));
-		assertThat(MenuMain.getComponents(), arrayContainingInAnyOrder(button));
-		Stream.of(button).forEach(this::assertActionListeners);
+		assertThat(MenuMain.getComponents(), arrayContainingInAnyOrder(allButton));
+		Stream.of(allButton).forEach(this::assertActionListeners);
+		verify(mockScheduler).scheduleAtFixedRate(Mockito.any(Runnable.class), anyLong(), anyLong(), Mockito.any(TimeUnit.class));
+		mockExecutors.close();
+	}
+	
+	JButton[] allButton() {
+		return new JButton[]{MenuMain.getItemGetButton(),
+			MenuMain.getItemDisposeButton(),
+			MenuMain.getCompositionButton(),
+			MenuMain.getSelectStageButton(),
+			MenuMain.getTestButton()
+		};
 	}
 	
 	void assertActionListeners(JButton button) {
