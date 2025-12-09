d309f59b391cda2e495934c77980d49b50456a1c
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-12-09 11:54:04 +0900

    Add files via upload

--- test/defendthecastle/MenuMainTest.java
+++ test/defendthecastle/MenuMainTest.java
@@ -1,5 +1,13 @@
 package defendthecastle;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.stream.Stream;
+
+import javax.swing.JButton;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +16,28 @@ class MenuMainTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		MenuMain = new MenuMain(new MainFrame());
+		
 	}
 
+	/**
+	 * 
+	 */
 	@Test
 	void testMenuMain() {
-		
+		MainFrame MainFrame = mock(MainFrame.class);
+		MenuMain = new MenuMain(MainFrame);
+		JButton[] button = {MenuMain.getItemGetButton(),
+				MenuMain.getItemDisposeButton(),
+				MenuMain.getCompositionButton(),
+				MenuMain.getSelectStageButton(),
+				MenuMain.getTestButton()
+		};
+		assertThat(MenuMain.getMainFrame(), is(MainFrame));
+		assertThat(MenuMain.getComponents(), arrayContainingInAnyOrder(button));
+		Stream.of(button).forEach(this::assertActionListeners);
+	}
+	
+	void assertActionListeners(JButton button) {
+		assertThat(button.getActionListeners(), not(emptyArray()));
 	}
 }
