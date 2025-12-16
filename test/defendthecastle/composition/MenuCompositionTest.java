c32762be4df619a99f9a37fa84576ce3f4fa832f
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-12-16 10:06:58 +0900

    Add files via upload

---
+++ test/defendthecastle/composition/MenuCompositionTest.java
@@ -0,0 +1,19 @@
+package defendthecastle.composition;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MenuCompositionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
