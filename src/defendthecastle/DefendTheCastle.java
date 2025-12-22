2d6e676224c839bc68327c1ddb27e97428b62500
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2025-12-22 09:06:05 +0900

    Add files via upload

--- src/defendthecastle/DefendTheCastle.java
+++ src/defendthecastle/DefendTheCastle.java
@@ -4,6 +4,10 @@ import savedata.FileCheck;
 
 public class DefendTheCastle {
 	public static void main(String[] args) {
+		new DefendTheCastle();
+	}
+	
+	DefendTheCastle(){
 		new FileCheck();
 		new MainFrame().mainMenuDraw();
 	}
