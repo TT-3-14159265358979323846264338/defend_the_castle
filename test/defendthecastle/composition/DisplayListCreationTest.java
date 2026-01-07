a17fb0694d147c47d9e2190097ae3a12052c332f
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-01-07 19:13:59 +0900

    Add files via upload

---
+++ test/defendthecastle/composition/DisplayListCreationTest.java
@@ -0,0 +1,12 @@
+package defendthecastle.composition;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DisplayListCreationTest {
+	@Test
+	void test() {
+		
+	}
+}
