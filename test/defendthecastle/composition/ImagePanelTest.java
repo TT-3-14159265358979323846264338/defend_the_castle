f7c6862503fc5a2229a149b88d6ca68c08a11931
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-01-13 09:54:42 +0900

    Add files via upload

---
+++ test/defendthecastle/composition/ImagePanelTest.java
@@ -0,0 +1,23 @@
+package defendthecastle.composition;
+
+import static custommatcher.CustomMatcher.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ImagePanelTest {
+	
+	
+	
+	
+	@Test
+	void test() {
+		
+	}
+}
