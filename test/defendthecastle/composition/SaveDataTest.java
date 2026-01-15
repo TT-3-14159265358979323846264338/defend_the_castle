4946c4675e250232d5da7b26f534628aba1ec008
TT-3-14159265358979323846264338 <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-01-15 10:58:31 +0900

    Add files via upload

---
+++ test/defendthecastle/composition/SaveDataTest.java
@@ -0,0 +1,23 @@
+package defendthecastle.composition;
+
+import static custommatcher.CustomMatcher.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+class SaveDataTest {
+	@InjectMocks
+	private SaveData SaveData;
+	
+	/**
+	 * 
+	 */
+	@Test
+	void testSaveData() {
+		
+	}
+}
